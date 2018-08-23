d55a1bb9d0d9513f5fb9c3050611a34d31aa4b1b
Julien Chichignoud <julien.chichignoud@gmail.com>
2018-08-23 11:24:26 +0200

    Replace gedmo sluggable with JS implementation

--- Resources/public/js/content-type.js
+++ Resources/public/js/content-type.js
@@ -39,6 +39,23 @@ jQuery(function ($) {
         $.get(url, function (response) {
             $('.acb-modal-add-field .modal-body').html(response);
             $('.acb-modal-add-field').modal();
+
+            var slugInput = $('.acb-modal-add-field').find('.acb-slug');
+            var slugSource = $(slugInput.data('slug-source'));
+            var timer;
+            slugSource.on('change.acb', function () {
+                slugInput.val(generateSlug(this.value));
+            });
+            slugSource.on('keyup.acb', function () {
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    slugInput.val(generateSlug(slugSource.val()));
+                }, 300);
+            });
+            slugInput.on('change', function () {
+                slugSource.off('change.acb');
+                slugSource.off('keyup.acb');
+            });
         });
     });
 
@@ -102,5 +119,18 @@ jQuery(function ($) {
                 $(this).hide();
             }
         });
+    }
+
+    function generateSlug (value) {
+        // use speakingurl lib if available
+        if (typeof getSlug === 'function') {
+            return getSlug(value);
+        }
+        // simple custom slug generator for fallback
+        // 1) convert to lowercase
+        // 2) remove dashes and pluses
+        // 3) replace spaces with dashes
+        // 4) remove everything but alphanumeric characters and dashes
+        return value.toLowerCase().trim().replace(/-+/g, '').replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
     }
 });
