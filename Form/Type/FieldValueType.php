aa906d41620795d9153951a0d1b4c258ab2adec1
Julien Chichignoud <julien.chichignoud@gmail.com>
2022-08-31 18:35:59 +0200

    Display field type name in the edit form

--- Form/Type/FieldValueType.php
+++ Form/Type/FieldValueType.php
@@ -4,6 +4,8 @@ namespace Sherlockode\AdvancedContentBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FieldValueType extends AbstractType
@@ -15,6 +17,11 @@ class FieldValueType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $options['field_type']->buildContentFieldValue($builder);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['panel_label'] = 'field_type.' . $options['field_type']->getCode() . '.label';
     }
 
     /**
