ebef98279283d0cef33b77bc5f53e5cfdcb20163
Julien Chichignoud <julien.chichignoud@gmail.com>
2022-09-15 11:11:43 +0200

    Improve UI for the edit field form

--- Resources/js/slide.js
+++ Resources/js/slide.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 class Slide
 {
     constructor() {
-        this.element = $('<div class="acb-lateral-slide"><button type="button" class="btn-close">x</button><div class="acb-lateral-slide-header"></div><div class="acb-lateral-slide-content"></div></div>');
+        this.element = $('<div class="acb-lateral-slide"><button type="button" class="btn-close"><i class="glyphicon glyphicon-remove"></i></button><div class="acb-lateral-slide-header"></div><div class="acb-lateral-slide-content"></div></div>');
         this.slideLayer = $('<div class="acb-lateral-slide-layer"></div>');
         this.header = this.element.find('.acb-lateral-slide-header');
         this.content = this.element.find('.acb-lateral-slide-content');
