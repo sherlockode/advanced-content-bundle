26de88a47308f852adb99d6866b501c44292ce12
Julien Chichignoud <julien.chichignoud@gmail.com>
2022-11-07 15:45:03 +0100

    Remove obsolete code from PageListener

--- EventListener/PageListener.php
+++ EventListener/PageListener.php
@@ -3,30 +3,10 @@
 namespace Sherlockode\AdvancedContentBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Sherlockode\AdvancedContentBundle\Manager\PageManager;
-use Sherlockode\AdvancedContentBundle\Model\ContentInterface;
 use Sherlockode\AdvancedContentBundle\Model\PageInterface;
 
 class PageListener
 {
-    /**
-     * @var PageManager
-     */
-    private $pageManager;
-
-    /**
-     * @var bool
-     */
-    private $shouldFlush = false;
-
-    /**
-     * @param PageManager $pageManager
-     */
-    public function __construct(PageManager $pageManager)
-    {
-        $this->pageManager = $pageManager;
-    }
-
     /**
      * @param LifecycleEventArgs $args
      */
@@ -41,38 +21,5 @@ class PageListener
         if ($object->getStatus() === null) {
             $object->setStatus(PageInterface::STATUS_DRAFT);
         }
-    }
-
-    /**
-     * @param LifecycleEventArgs $args
-     */
-    public function preUpdate(LifecycleEventArgs $args)
-    {
-        $object = $args->getObject();
-
-        if (!$object instanceof PageInterface) {
-            return;
-        }
-
-        $this->shouldFlush = $this->pageManager->updateContentForPage($object);
-    }
-
-    /**
-     * @param LifecycleEventArgs $args
-     */
-    public function postUpdate(LifecycleEventArgs $args)
-    {
-        $object = $args->getObject();
-
-        if (!$object instanceof PageInterface) {
-            return;
-        }
-
-        if (!$this->shouldFlush) {
-            return;
-        }
-
-        $entityManager = $args->getEntityManager();
-        $entityManager->flush();
     }
 }
