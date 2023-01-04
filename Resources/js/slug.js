1912a58b7933cec8f5ba0a9dbd1807699029ef05
sophie-mulard <smulard@gmail.com>
2023-01-04 18:11:42 +0100

    Improve fallback slugify

--- Resources/js/slug.js
+++ Resources/js/slug.js
@@ -28,10 +28,32 @@ function generateSlug (value) {
     }
     // simple custom slug generator for fallback
     // 1) convert to lowercase
-    // 2) remove dashes and pluses
-    // 3) replace spaces with dashes
-    // 4) remove everything but alphanumeric characters and dashes
-    return value.toLowerCase().trim().replace(/-+/g, '').replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
+    // 2) replace pluses, quotes and spaces with dashes
+    // 3) replace consecutive dashes with a single one
+    // 4) remove trailing dashes
+    // 5) replace special chars with their slug friendly equivalent
+    // 6) remove everything but alphanumeric characters and dashes
+    return value
+      .toLowerCase().trim()
+      .replace(/[+'"\s]/g, '-')
+      .replace(/-{2,}/g, '-')
+      .replace(/^-/g, '')
+      .replace(/-$/g, '')
+
+      .replace(/[àáâãäå]/g, 'a')
+      .replace(/æ/g, 'ae')
+      .replace(/ç/g, 'c')
+      .replace(/[èéêë]/g, 'e')
+      .replace(/[ìíîï]/g, 'i')
+      .replace(/ñ/g, 'n')
+      .replace(/[òóôõö]/g, 'o')
+      .replace(/œ/g, 'oe')
+      .replace(/ß/g, 'ss')
+      .replace(/[ùúûüµ]/g, 'u')
+      .replace(/[ýÿ]/g, 'y')
+
+      .replace(/[^a-z0-9-]/g, '')
+    ;
 }
 
 jQuery(function ($) {
